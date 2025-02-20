c84329192cf1bf85c71dbbe6a9f3efa4b37f7727
Noah Colvin <7552226+noahcolvin@users.noreply.github.com>
2025-02-19 17:11:31 -0700

    Adding search bar on store page

--- app/src/main/java/com/example/android/furry/ui/StoreScreenViewModel.kt
+++ app/src/main/java/com/example/android/furry/ui/StoreScreenViewModel.kt
@@ -15,8 +15,23 @@ class StoreScreenViewModel : ViewModel() {
     private val _storeItemsList = MutableStateFlow<List<StoreItem>?>(null)
     val storeItemsList: StateFlow<List<StoreItem>?> get() = _storeItemsList.asStateFlow()
 
+    private val _searchText = MutableStateFlow("")
+    val searchText: StateFlow<String> get() = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching: StateFlow<Boolean> get() = _isSearching.asStateFlow()
+
     init {
         getStoreItemsList()
+    }
+
+    fun onSearchTextChanged(text: String) {
+        _searchText.value = text
+        getStoreItemsList(search = text)
+    }
+
+    fun onSearchChanged(searching: Boolean) {
+        _isSearching.value = searching
     }
 
     fun getStoreItemsList(
